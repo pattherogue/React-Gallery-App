ea0630b6db59d199ef524602c1cb39b74b86c0c8
Paolo Gomez <peweegardner248@gmail.com>
2021-12-08 09:29:53 -0800

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,2 +1,12 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
+
+class Search extends Component {
+    state = {
+        querySearch: ''
+    }
+}
+
+onSearch = e => {
+    this.setState({querySearch: e.target.value });
+}
