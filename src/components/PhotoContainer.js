0c68c12d5a2f16bd32b29931a164a1acf8c33029
Paolo Gomez <peweegardner248@gmail.com>
2021-12-21 09:06:36 -0800

    Update PhotoContainer.js

--- src/components/PhotoContainer.js
+++ src/components/PhotoContainer.js
@@ -2,3 +2,16 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Photo from '.Photo';
 
+class PhotoContainer extends Component {
+    pictureSearch() {
+        let photos = this.props.imageData.map( image => 
+            <Photo 
+                url={`https://live.staticflickr.com/${image.server}/${image.id}_${image.secret}_w.jpg`} 
+                key={image.id} 
+                alt={image.title}
+            />
+        );
+
+        return photos;
+    }
+}
