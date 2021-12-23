499c5aaa6e6dfb79d1ec5713a6fe662e122b3278
Paolo Gomez <peweegardner248@gmail.com>
2021-12-23 11:58:07 -0800

    update

--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,6 @@
+import './css/index.css';
+import SearchForm from './components/SearchForm';
+
 import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
@@ -9,7 +12,7 @@ import apiKey from '../config';
 
 import Nav from './Nav';
 import NotFound from './NotFound';
-import Search from './Search';
+import Search from './SearchForm';
 import PhotoContainer from './PhotoContainer';
 
 class App extends Component {
