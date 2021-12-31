0c69caa97186d127362cac925b19c153f92ed200
Paolo Gomez <peweegardner248@gmail.com>
2021-12-31 10:40:31 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,15 +1,10 @@
-import './css/index.css';
-
-
-/* Nav data */
-import ApiKey from './config';
-
 import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Route,
     Switch
 } from 'react-router-dom';
+import './css/index.css';
 
 //Components 
 import Nav from './src/components/Nav';
