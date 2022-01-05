8334d235d45cb13aaf84bd4fa5bdbcc6a67ea86c
Paolo Gomez <peweegardner248@gmail.com>
2022-01-05 08:55:56 -0800

    update

---
+++ Components/NotFound.js
@@ -0,0 +1,12 @@
+import React from '../search-app/node_modules/react';
+
+const NotFound = () => {
+    return (
+    <li class="not-found">
+        <h3>No Results Found</h3>
+        <p>You search did not return any results. Please try again.</p>
+    </li>
+    )
+};
+
+export default NotFound;
