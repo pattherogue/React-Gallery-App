b9da4363e3096df4a7ca7dc732206d8b9abab1eb
Paolo Gomez <peweegardner248@gmail.com>
2022-01-06 11:36:15 -0800

    Update App.js

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -4,6 +4,12 @@ import { BrowswerRouter, Route, Switch , Redirect } from 'react-router-dom';
 import './App.css';
 import apiKey from './config';
 
+//Components
+import Nav from './Components/Nav';
+import PhotoContainer from './Components/PhotoContainer';
+import SearchForm from './Components/SearchForm';
+import NotFound from './Components/NotFound';
+
 function App() {
   return (
     <div className="App">
