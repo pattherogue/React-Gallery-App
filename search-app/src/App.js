5f00d68d7e290b7e2ffd51ae89f9f4355dc40fe8
Paolo Gomez <peweegardner248@gmail.com>
2022-01-10 11:34:25 -0800

    Update App.js

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -38,7 +38,8 @@ class App extends Component {
 
   performSearch = (query) => {
     this.setState({loading: true})
-    axios.get(``).then(response => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q={query}&limit=24&api_key=dc6zaTOxFJmzC 
+    `).then(response => {
 
       if(query === 'nature') {
         this.setState({
