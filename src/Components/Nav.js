b1ebf2d6af8a20e6ec50c67fd3d06fa35258fb24
Paolo Gomez <peweegardner248@gmail.com>
2022-01-15 13:57:42 -0800

    Update Nav.js

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -4,11 +4,11 @@ import { NavLink } from 'react-router-dom';
 const Nav = () => {
     return (
         <nav class="main-nav">
-        <ul>
-          <li><NavLink to="/nature">Nature</NavLink></li>
-          <li><NavLink to="/food">Food</NavLink></li>
-          <li><NavLink to="/creative">Creative</NavLink></li>
-        </ul>
+          <ul>
+            <li><NavLink to="/nature">Nature</NavLink></li>
+            <li><NavLink to="/food">Food</NavLink></li>
+            <li><NavLink to="/creative">Creative</NavLink></li>
+          </ul>
       </nav>
     );
 }
