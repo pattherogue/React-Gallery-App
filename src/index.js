1867076a02dc9d978e7b3e334e38713c13d8b990
Paolo Gomez <peweegardner248@gmail.com>
2022-01-19 08:41:36 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowswerRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <App />,
+  
   document.getElementById('root')
 );
 
