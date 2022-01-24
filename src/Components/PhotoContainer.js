71213f6d3ef23e1bc3d430fb72f82f4084e75708
Paolo Gomez <peweegardner248@gmail.com>
2022-01-24 13:05:03 -0800

    Update PhotoContainer.js

--- src/Components/PhotoContainer.js
+++ src/Components/PhotoContainer.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Photo from './Photo';
-import NotFound from './NotFound';
-import Loading from './Loading';
 
 class PhotoContainer extends Component {
     pictureSearch() {
